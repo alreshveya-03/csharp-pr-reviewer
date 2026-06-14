f084e269c688f765e3f7c1e523f654d979c80246
Rithanyavijaykumar <rithanyavijaykumar04@gmail.com>
2026-06-14 12:34:24 +0530

    Create code.cs

---
+++ code.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number1 = 10;
+        int number2 = 0;
+
+        int result = number1 / number2;
+
+        Console.WriteLine("Result: " + result);
+    }
+}
