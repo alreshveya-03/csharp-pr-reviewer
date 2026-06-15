aebf92b94453e7384b7e92039e13bdb71b7f8d96
Yashirahamed <yashir27ahamed@gmail.com>
2026-06-15 21:22:10 +0530

    Create zero.cs

---
+++ zero.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 10;
+        int b = 0;
+
+        int result = a / b; // Runtime error
+        Console.WriteLine(result);
+    }
+}
